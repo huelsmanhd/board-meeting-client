94fa934fd7e1a92ebde1ca56839545e3017266a8
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-13 17:46:53 -0400

    nav bar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from "../token.service";
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  sessionToken: string = "";
+  admin: string = "";
+  
 
-  constructor() { }
+  logoutButton = false;
+  constructor(
+    private token: TokenService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.sessionToken = this.token.getToken();
+    this.admin = this.token.getAdmin();
+
   }
 
+  logoutView() {
+    if(this.token.sessionToken === "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  logOut() {
+    sessionStorage.clear();
+    this.router.navigate(["/home"]);
+    this.token.clearSessionToken();
+     
+  }
 }
