2e024786463076c788d343009cbf0fa770941ce6
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-14 09:15:08 -0400

    nav changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbCarouselConfig } from "@ng-bootstrap/ng-bootstrap";
 
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ NgbCarouselConfig ]
 })
 export class HomeComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  constructor() { }
+  constructor(config: NgbCarouselConfig) { 
+    config.interval = 2000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+   }
 
   ngOnInit() {
   }
