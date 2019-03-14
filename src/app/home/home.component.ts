3ab5486d34f9e1b9ddc47b5e796405b50ebb2ff1
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-14 14:34:15 -0400

    storing username

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import { NgbCarouselConfig } from "@ng-bootstrap/ng-bootstrap";
-
-=======
->>>>>>> 498e5f27eb01cb50e3a6a214b76a9269e4cf6c98
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-<<<<<<< HEAD
-  // providers: [ NgbCarouselConfig ]
-})
-export class HomeComponent implements OnInit {
-  constructor(/*config: NgbCarouselConfig*/) { 
-    // config.interval = 2000;
-    // config.wrap = true;
-    // config.keyboard = false;
-    // config.pauseOnHover = false;
-=======
 })
 export class HomeComponent implements OnInit {
   constructor() {
->>>>>>> 498e5f27eb01cb50e3a6a214b76a9269e4cf6c98
    }
 
   ngOnInit() {
