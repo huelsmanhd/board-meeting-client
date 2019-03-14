bf62a3da57cc1716fdc0225090e7f0e4f2c7bc04
Brent <brent@Brents-MacBook-Pro.local>
2019-03-14 16:47:29 -0400

    get all events

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type" : "application/json"
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllEvents(){
+
+    let baseURL = 'https://board-meeting-sever.herokuapp.com/event/all';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type" : "application/json"
+      })
+    }
+
+    return this.http.get(baseURL, httpOptions)
+
+
+
+    fetch('https://board-meeting-sever.herokuapp.com/event/all', {
+      method: "GET",
+      headers: new Headers({ "Content-Type": "application/json"})
+    })
+  }
+  
 }
