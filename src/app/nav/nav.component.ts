81780529b239675a464cc6e0199f6523df796098
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-20 16:49:02 -0400

    https

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -45,7 +45,7 @@ export class NavComponent implements OnInit {
   viewCreateEvent() {
     // if (window.location.href === 'http://localhost:4200/home') {
     
-    if (window.location.href === 'http://board-meeting-client.herokuapp.com/home') {
+    if (window.location.href === 'https://board-meeting-client.herokuapp.com/home') {
     return false;
   } else {
     return true;
